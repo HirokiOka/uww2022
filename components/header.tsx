02b513921730bf46561ff34b09aa8dd0a2f81630
hiroka <mementoka@gmail.com>
2023-10-27 14:58:56 +0900

    modified link

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <>
     <header className='bg-gray-200 top-0 fixed w-full'>
       <nav className='m-3 text-xl text-right'>
-        <Link href="./index" className='m-2 hover:underline'>Home</Link>
-        <Link href="./program" className='m-2 hover:underline'>Program</Link>
+        <Link href="http://s2-lab.sakura.ne.jp/uww2022/index.html" className='m-2 hover:underline'>Home</Link>
+        <Link href="http://s2-lab.sakura.ne.jp/uww2022/program.html" className='m-2 hover:underline'>Program</Link>
         <Link href="http://www.yumebutai.org/access/index.html" className='m-2 hover:underline'>Access</Link>
       </nav>
     </header>
